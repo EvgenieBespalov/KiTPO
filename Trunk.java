19240fd913ff86d38c2b4c30e5e3f107929d5be3
EvgenieBespalov <95974491+EvgenieBespalov@users.noreply.github.com>
2021-12-11 23:06:58 +0700

    Delete Trunk.java

--- Trunk.java
+++
@@ -1,9 +0,0 @@
-package kitpolw1;
-
-public interface Trunk<T>
-{
-    void insertNode(T value, Comparator comparator);
-    boolean deleteNodeByInd(int index);
-    void printTree();
-    void createCompleteBST();
-}
